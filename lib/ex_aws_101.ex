2ba661efbedc40c0fe7532c651d7ed5ecb831322
kooo5252 <masaki.affiliate@gmail.com>
2018-07-23 13:33:39 +0900

    内田＿提出課題
    この1ファイルのみ編集しました。

--- lib/ex_aws_101.ex
+++ lib/ex_aws_101.ex
@@ -15,4 +15,66 @@ defmodule ExAws101 do
   def hello do
     :world
   end
+
+#------------------------------------------------------------
+
+  defmodule S3 do
+  
+    def put_object(bucket, object, body) do
+  
+      ExAws.S3.put_object(bucket, object, body)
+      |> ExAws.request()
+  
+    end
+
+    def put_object!(bucket, object, body) do
+  
+      case ExAws.S3.put_object(bucket, object, body)
+      |> ExAws.request() do
+        
+        {:ok, _} -> %{status_code: 200}
+      end
+  
+    end
+
+    def put_object_acl(bucket, object, acl) do
+  
+      ExAws.S3.put_object_acl(bucket, object, acl)
+      |> ExAws.request()
+  
+    end
+
+    def delete_object(bucket, object) do
+  
+      ExAws.S3.delete_object(bucket, object)
+      |> ExAws.request()
+  
+    end
+
+    def put_bucket(bucket, region) do
+  
+      ExAws.S3.put_bucket(bucket, region)
+      |> ExAws.request()
+  
+    end
+
+    def force_delete_bucket(bucket) do
+
+      %{body: %{contents: contents}}
+      = ExAws.S3.list_objects(bucket)
+      |> ExAws.request!()
+
+
+      objects = contents
+      |> Enum.map(& &1.key)
+
+      ExAws.S3.delete_all_objects(bucket, objects)
+      |> ExAws.request!()
+
+      ExAws.S3.delete_bucket(bucket)
+      |> ExAws.request()
+  
+    end
+
+  end
 end
